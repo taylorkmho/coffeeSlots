d244edf0cacdffde652137a526d6814f083afbd1
Taylor Ho <taylorkmho@gmail.com>
2016-04-28 19:01:42 -0700

    Added src files (html, css, javascript)

---
+++ src/assets/js/application.js
@@ -0,0 +1,3 @@
+require('es6-shim');
+import { addClass, removeClass, hasClass } from "./lib/_helpers";
+require('./lib/_nodelist-shim');
---
+++ src/assets/js/lib/_helpers.js
@@ -0,0 +1,23 @@
+// Ref: http://youmightnotneedjquery.com
+export function addClass(el, className) {
+  if (el.classList) {
+    el.classList.add(className);
+  } else {
+    var currentClassName = el.className;
+    el.setAttribute('class', currentClassName + ' ' + className);
+  }
+}
+
+// Ref: http://youmightnotneedjquery.com
+export function removeClass(el, className) {
+  if (el.classList) {
+    el.classList.remove(className);
+  } else {
+    el.setAttribute('class', el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' '));
+  }
+}
+
+// Ref: http://youmightnotneedjquery.com
+export function hasClass(el, className) {
+  return el.className && new RegExp("(\\s|^)" + className + "(\\s|$)").test(el.className);
+}
