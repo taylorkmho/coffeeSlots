8f0c6b284264df744da22c7627fafd52e0ec496e
Taylor Ho <taylorkmho@gmail.com>
2016-04-29 00:19:17 -0700

    Generating slot elements via javascript

--- src/assets/js/application.js
+++ src/assets/js/application.js
@@ -1,3 +1,33 @@
 require('es6-shim');
 import { addClass, removeClass, hasClass } from "./lib/_helpers";
 require('./lib/_nodelist-shim');
+
+let caffeineData = [
+  {
+    type: 'coffee',
+    components: ['maker', 'filter', 'ground']
+  },
+  {
+    type: 'tea',
+    components: ['pot', 'strainer', 'loose']
+  },
+  {
+    type: 'espresso',
+    components: ['machine', 'tamper', 'ground']
+  }
+]
+
+/*
+  for each slot element (from slots array)
+    iterate over caffeineData
+      append div containing data to slot element
+*/
+let slots       = Array.from(document.querySelectorAll('.slots__slot'));
+slots.forEach((slotEl,slotIndex)=>{
+  caffeineData.forEach((data)=>{
+    let slotOption = document.createElement('div');
+    addClass(slotOption, 'slots__option');
+    slotOption.innerHTML = data.type + ' <span>' + data.components[slotIndex] + '</span>';
+    slotEl.appendChild(slotOption);
+  })
+})
